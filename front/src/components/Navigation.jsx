6a5b840e7145486cdad5e21be7cf7c068bef1cdf
Lesandrod <lentrix12345@gmail.com>
2023-06-20 19:56:53 -0500

    navbar

--- front/src/components/Navigation.jsx
+++ front/src/components/Navigation.jsx
@@ -2,38 +2,46 @@ import Link from "next/link"
 
 const links = [
     {
-        text:"Home",
-        href:"/"
+        text: "Home",
+        href: "/"
     },
     {
         text: "Ayuda",
-        href:"/ayuda"
+        href: "/ayuda"
     },
     {
         text: "Juegos",
-        href:"/juegos"
+        href: "/juegos"
     }
-    
+
 ]
 
 export function Navigation() {
 
     return (
-        <header>
-            <nav>
-                <div>
+        <header className="mt-4">
+            <nav className="flex justify-around items-center">
+                <div className="flex justify-between w-full md:w-auto">
                     <h1>Logo</h1>
                 </div>
                 <div>
-                    <ul>
+                    <ul className="flex gap-10 items-center justify-center">
                         {
-                        links.map((link)=>(
-                            <li>
-                                <Link href={link.href}>{link.text}</Link>
-                            </li>
-                        ))
+                            links.map((link) => (
+                                <li>
+                                    <Link href={link.href} className="hover:text-purple-800 font-semibold" >
+                                        {link.text}
+                                    </Link>
+                                </li>
+                            ))
                         }
                     </ul>
+                </div>
+                <div className="flex gap-5 items-center">
+                    <div>
+                        <button className="underline">Sign In</button>
+                    </div>
+                   
                 </div>
             </nav>
         </header>
