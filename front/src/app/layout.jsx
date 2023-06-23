28194f642e5a80371edaff2b9ebda3a715ecdef1
Lesandrod <lentrix12345@gmail.com>
2023-06-22 22:38:19 -0500

    landing v2

--- front/src/app/layout.jsx
+++ front/src/app/layout.jsx
@@ -3,11 +3,16 @@ import Head from "next/head";
 export default function RootLayout({ children }) {
   return (
     <html >
-      <Head>
+      <head >
         <title>Tesis</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body >      
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300&display=swap" rel="stylesheet"></link>
+
+
+      </head>
+      <body >
         {children}
       </body>
     </html>
